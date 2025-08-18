07f22be3c0f02b09ca10726ddf0d04d08bdabc7e
miderror <miderror.dev@gmail.com>
2025-08-18 13:47:58 +0300

    add ru verbose in apps

--- backend/checker/apps.py
+++ backend/checker/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class CheckerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'backend.checker'
+    verbose_name = 'Проверки кода'
